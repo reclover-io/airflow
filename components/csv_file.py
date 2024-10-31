78f0246ad7febc6b35b54b3e28adc2d2ff849421
PCTangmo <chadaphornt20@gmail.com>
2024-10-31 12:05:15 +0700

    edit to origin project

--- components/csv_file.py
+++
@@ -1,5 +0,0 @@
-def csv_file(**kwargs):
-    # Dummy task to simulate moving a file
-    print("Simulating file move...")
-    # ในการใช้งานจริง คุณอาจจะใช้ shutil.move() หรือวิธีการอื่นๆ สำหรับการย้ายไฟล์
-    pass  # Do nothing for now
