c0d9a07a707854c0ee01da125b4fabb5062c54e4
PCTangmo <chadaphornt20@gmail.com>
2024-11-06 23:38:00 +0700

    update .sh

--- script_bash/generate_dag.sh
+++ script_bash/generate_dag.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# รับค่า Arguments
 API_URL="$1"
 TEMP_DIR="$2"
 OUTPUT_DIR="$3"
@@ -9,7 +8,6 @@ API_HEADERS="$5"
 DAG_NAME="$6"
 DEFAULT_CSV_COLUMNS="$7"
 
-# สร้างไฟล์ DAG ใหม่โดยแทนที่ค่า Placeholder ในเทมเพลต
 sed \
     -e "s|{API_URL}|$API_URL|g" \
     -e "s|{TEMP_DIR}|$TEMP_DIR|g" \
@@ -18,6 +16,12 @@ sed \
     -e "s|{API_HEADERS}|$API_HEADERS|g" \
     -e "s|{DAG_NAME}|$DAG_NAME|g" \
     -e "s|{DEFAULT_CSV_COLUMNS}|$DEFAULT_CSV_COLUMNS|g" \
-    dag_template.py > /opt/airflow/dags/$DAG_NAME.py
+    dag_template.py > ../dags/$DAG_NAME.py
 
-echo "DAG file /opt/airflow/dags/$DAG_NAME.py created successfully."
+if [[ -f "/opt/airflow/dags/$DAG_NAME.py" ]]; then
+    echo "DAG file /opt/airflow/dags/$DAG_NAME.py created successfully."
+else
+    echo "Error: Failed to create DAG file /opt/airflow/dags/$DAG_NAME.py"
+    exit 1
+fi
+
--- script_bash/run_generate_dag.sh
+++ script_bash/run_generate_dag.sh
@@ -1,11 +1,11 @@
-# รันสคริปต์ generate_dag.sh พร้อมค่าพารามิเตอร์
-# API URL ที่ต้องการดึงข้อมูล
-# TEMP_DIR: โฟลเดอร์ชั่วคราวสำหรับเก็บไฟล์
-# OUTPUT_DIR: โฟลเดอร์สำหรับไฟล์ผลลัพธ์
-# CONTROL_DIR: โฟลเดอร์สำหรับเก็บไฟล์ควบคุม
-# API_HEADERS: ค่า Authorization ของ API Headers
-# DAG_NAME: ชื่อ DAG ที่ต้องการตั้ง (จะเป็นชื่อไฟล์ DAG ด้วย)
-# DEFAULT_CSV_COLUMNS: คอลัมน์ของ CSV ที่ต้องการให้แสดง
+# Run tonight Generate_dag.sh with the following values
+# API_URL: Fetch data
+# TEMP_DIR: Temporary important to store files
+# OUTPUT_DIR: For output files
+# CONTROL_DIR: For control files
+# API_HEADERS: API header authentication
+# DAG_NAME: DAG name to be created
+# DEFAULT_CSV_COLUMNS: Request CSV to display
 
 ./generate_dag.sh \
     "http://34.124.138.144:8000/mobileAppActivity" \
@@ -13,5 +13,5 @@
     "/opt/airflow/output" \
     "/opt/airflow/output/control" \
     "R2pDZVNaRUJnMmt1a0tEVE5raEo6ZTNrYm1WRk1Sb216UGUtU21DS21iZw==" \
-    "Friend_MB_Noti_Spending" \
+    "TEST_Friend_MB_Noti_Spending" \
     "['RequestID', 'Path', 'UserToken', 'RequestDateTime', '_id', 'Status', 'CounterCode']"
