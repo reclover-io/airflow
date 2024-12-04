657a1e91931f5e4798e6f1d391bba3c1602eac5a
Mirrorsan <booranasak.k@cs.rmu.ac.th>
2024-12-04 15:46:12 +0700

    Change Line Token Here.

--- components/constants.py
+++ components/constants.py
@@ -9,5 +9,5 @@ DB_CONNECTION = 'postgresql+psycopg2://airflow:airflow@postgres:5432/airflow'
 # Other Constants
 PAGE_SIZE = 50
 
-ACCESS_TOKEN = "9T/JcMcnS9+pqbdlzZI8vW/UKBldvWeh/2l8CR2D1yNKuS+vawXn9X7IesPRDuslaKECFSUWVwOzgn95FWfzVktrZKp0R1RuQdVZWUJVZySJWyFfDfRL6bpc3nRC6z+8EVwsGfPmWirNaGEdd0u5uQdB04t89/1O/w1cDnyilFU="
-TO_USER_ID = "Uc0f3f827c4145c46392f3bb41a4d2b2c"
+ACCESS_TOKEN = "Enter Your Access Token Here."
+TO_USER_ID = "Enter Your user ID  Here."
