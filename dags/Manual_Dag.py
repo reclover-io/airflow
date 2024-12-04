1b70b781ca0eb83f159dc6ae72b681533608b4c1
Phurinat Kantapayao <phurinat.k@reclover.io>
2024-12-04 10:00:38 +0700

    edit flow manual dag

--- dags/Manual_Dag.py
+++ dags/Manual_Dag.py
@@ -148,8 +148,8 @@ with DAG(
         trigger_rule=TriggerRule.ALL_SUCCESS,
     )
 
-    validate_input >> [running_notification, failure_notification]
-    validate_input >> wait_for_start_time >> [process_task, failure_notification]
+    validate_input >> [failure_notification]
+    validate_input >> wait_for_start_time >> [running_notification, process_task, failure_notification]
     process_task >> [uploadtoFTP, failure_notification]
     uploadtoFTP >> [success_notification, failure_notification]
     process_task >> success_notification
